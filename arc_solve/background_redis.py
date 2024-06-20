25033a5f897f48a700f1e4e06acbda00976bacb5
rgreenblatt <greenblattryan@gmail.com>
2024-06-19 21:56:57 -0700

    fix bug

--- arc_solve/background_redis.py
+++ arc_solve/background_redis.py
@@ -10,7 +10,7 @@ def run_redis_in_background():
     redis_port = os.environ["REDIS_READER_PORT"]
 
     os.makedirs("./redis_dir", exist_ok=True)
-    shutil.copy(Path(__file__).parent / "redis.conf", "./redis_dir/redis.conf")
+    shutil.copy(Path(__file__).parent.parent / "redis.conf", "./redis_dir/redis.conf")
 
     # cd ./redis_dir && redis-server --port $REDIS_READER_PORT &> log.txt &
     shell_cmd = f"redis-server --port {redis_port} &> ./log.txt &; disown %"
