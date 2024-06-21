34307896e786f8dcf961c9e16487bd66130c434a
rgreenblatt <greenblattryan@gmail.com>
2024-06-20 18:43:19 -0700

    misc fix

--- arc_solve/load_data.py
+++ arc_solve/load_data.py
@@ -10,7 +10,12 @@ with open(json_file_path, "r") as file:
 
 loaded_names = list(out_data_by_name_d.keys())
 
-for file in os.listdir("ARC-AGI/data/training"):
-    with open("ARC-AGI/data/training/" + file) as f:
-        data = json.load(f)
-        out_data_by_name_d[file] = data
+train_json_file_path = os.environ.get(
+    "INPUT_TRAIN_JSON", "/kaggle/input/arc-prize-2024/arc-agi_training_challenges.json"
+)
+
+with open(train_json_file_path, "r") as file:
+    train_data = json.load(file)
+
+# backward compatibility hack
+out_data_by_name_d.update({name + ".json": v for name, v in train_data.items()})
